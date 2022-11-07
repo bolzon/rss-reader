49db9c6bcda1f843a1bf675ea4a6998f92fd56b9
Alexandre Bolzon <blzn@mail.ru>
2022-11-07 10:41:32 +0100

    add db tx

--- src/rss/reader/db/provider.py
+++ src/rss/reader/db/provider.py
@@ -1,6 +1,11 @@
 import logging
 
+from typing import Callable
+
 from pymongo import MongoClient
+from pymongo.client_session import ClientSession
+from pymongo.collection import Collection
+from pymongo.errors import ConnectionFailure, OperationFailure
 
 
 logger = logging.getLogger(__name__)
@@ -8,10 +13,40 @@ logger = logging.getLogger(__name__)
 
 class DbProvider:
     def __init__(self, url: str, dbname: str):
-        self.client = MongoClient(url)
-        self.db = self.client[dbname]
+        self.client: MongoClient = MongoClient(url)
+        self.db = self.client.get_database(dbname)
         logger.info('Connected to DB: %s (%s)', url, dbname)
 
     def close(self):
         logger.info('Closing DB connection')
-        self.db.close()
+        self.client.close()
+
+
+class DbTransaction:
+    def __init__(self, db_provider: DbProvider, tx: Callable):
+        self.db_provider = db_provider
+        self.tx = tx
+
+    def execute(self):
+        session = self.db_provider.client.start_session()
+        db_txctx = DbTransactionContext(self, session)
+        try:
+            with session.start_transaction():
+                self.tx(db_txctx)
+                session.commit_transaction()
+        except (ConnectionFailure, OperationFailure) as e:
+            raise DbTransaction(e)
+
+
+class DbTransactionContext:
+    def __init__(self, db_tx: DbTransaction, session: ClientSession):
+        self.db_tx = db_tx
+        self.session: ClientSession = session
+
+    def get_collection(self, name: str, db_name: str) -> Collection:
+        return self.session.client.get_database(db_name).get_collection(name)
+
+
+class DbTransactionError(RuntimeError):
+    def __init__(self, msg: str, parent_ex: Exception):
+        super().__init__(**parent_ex)
