8ddcb2e9a3ca499989a95f1693068297f073921a
Alexandre Bolzon <blzn@mail.ru>
2022-11-09 18:25:00 +0100

    add signupuser model to signup user

--- src/rss/reader/routers/signup.py
+++ src/rss/reader/routers/signup.py
@@ -7,6 +7,7 @@ from fastapi.encoders import jsonable_encoder
 from rss.reader.auth.pwd import encrypt_user_pwd
 from rss.reader.db.repository.user import UserRepository
 from rss.reader.domain.user import User, UserWithPassword
+from rss.reader.models.user import SignupUser
 
 
 logger = logging.getLogger(__name__)
@@ -15,10 +16,10 @@ router = APIRouter()
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=User)
-def signup(request: Request, user: UserWithPassword):
+def signup(request: Request, user: SignupUser):
     user.password = encrypt_user_pwd(user.password)
     user_repo: UserRepository = request.app.repository.user
-    json_user = jsonable_encoder(user)
-    db_user = User(**user_repo.create(json_user))
+    domain_user = UserWithPassword(**jsonable_encoder(user))
+    db_user = user_repo.create(jsonable_encoder(domain_user))
     logger.debug('User created: %s', db_user)
-    return db_user
+    return User(**db_user)
