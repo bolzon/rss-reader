bdea1571c32178e39608da015ae4cc5da7cdb8ad
Alexandre Bolzon <blzn@mail.ru>
2022-11-12 23:22:05 +0100

    inject repo into users route

--- src/rss/reader/routers/user.py
+++ src/rss/reader/routers/user.py
@@ -1,9 +1,10 @@
 import logging
 
-from fastapi import APIRouter, HTTPException, Request, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from rss.reader.db.repository import Repository
 
-from rss.reader.db.repository.user import UserRepository
 from rss.reader.domain.user import User, UserList
+from rss.reader.injections.repo import db_repo
 from rss.reader.models.not_found import NotFound
 
 
@@ -13,16 +14,14 @@ router = APIRouter()
 
 
 @router.get('/', response_model=UserList)
-def get_users(request: Request):
-    user_repo: UserRepository = request.app.repository.user
-    return UserList(users=user_repo.get_all())
+def get_users(repo: Repository = Depends(db_repo)):
+    return UserList(users=repo.user.get_all())
 
 
 @router.get('/{id}', response_model=User,
             responses={status.HTTP_404_NOT_FOUND: {'model': NotFound}})
-def get_user(request: Request, id: str):
-    user_repo: UserRepository = request.app.repository.user
-    db_user = user_repo.get(filter={'_id': id})
+def get_user(id: str, repo: Repository = Depends(db_repo)):
+    db_user = repo.user.get(filter={'_id': id})
     if not db_user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'User with id "{id}" not found')
